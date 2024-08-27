e0ddf0edc8e5bbc1f83ab1e955fe5d166c80fbdd
syedanastanweer <syedanastanwer@gmail.com>
2024-08-27 22:05:20 +0500

    commit6

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,10 @@
 import React, { VFC } from 'react';
-import { LinkIconButton } from './LinkIconButton';
 import { TCanvas } from './three/TCanvas';
 
 export const App: VFC = () => {
-	return (
-		<div className='appmaindiv'>
-			<TCanvas />
-			{/* <LinkIconButton
-				imagePath="/assets/icons/github.svg"
-				linkPath="https://github.com/nemutas/r3f-gyroid-sphere-operable"
-			/> */}
-		</div>
-	)
+  return (
+    <div className='appmaindiv'>
+      <TCanvas />
+    </div>
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,52 +1,65 @@
 import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { App } from './components/App';
+import { AboutPage } from './components/AboutPage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className='indexmaindiv'>
-      <App />
-      <section className="section-home websection">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              {/* Any content you want within the section */}
-            </div>
-          </div>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="main-title">
-                <h2>Pulverize the stigma</h2>
+    <Router>
+      <div className='indexmaindiv'>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about-us" element={<AboutPage />} />
+        </Routes>
+        {/* This section applies globally */}
+        <section className="sectionmain">
+          <div className="section-home-one">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  {/* Any content you want within the section */}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <div className="main-title-two">
-                <h2>Navigating the pathways to well-being</h2>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="main-title">
+                    <h2>Pulverize the stigma</h2>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="navnew">
-              <div className="navnew-link">
-                <div className="link-anchore">
-                  <a href="about-us">About Jon</a>
-                  <a href="press">Press</a>
-                  <a href="faq">Faq's</a>
-                  <a href="testimonials">Reviews</a>
-                  <a href="contact-us">Contact Me</a>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="main-title-two">
+                    <h2>Navigating the pathways to well-being</h2>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+          <div className="section-home-two">
+            <div className="row">
+              <div className="navnew">
+                <div className="navnew-link">
+                  <div className="link-anchore">
+                    <a href="/about-us" className="nav-link">About Jon</a>
+                    <a href="/press">Press</a>
+                    <a href="/faq">Faq's</a>
+                    <a href="/testimonials">Reviews</a>
+                    <a href="/contact-us">Contact Me</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
